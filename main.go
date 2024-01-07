9ed21af6f2fe7b67ea416ecdf3be73db66a66254
Masud Rana <masudranaata@gmail.com>
2024-01-08 00:13:38 +0600

    Project initialization

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/M45UDrana/go-crud/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
 
 func main() {
-	fmt.Println("Hello, Masud")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, Masud",
+		})
+	})
+
+	r.Run()
 }
